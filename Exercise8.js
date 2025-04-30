c9a4c2aa3052b4bd80a78ded81fe8aa123dea871
JamesP-GitH <james.pagliaccio@gmail.com>
2025-05-01 09:52:22 +1000

    2nd test case

--- Exercise8.js
+++ Exercise8.js
@@ -9,11 +9,25 @@ function testDice6() {
     for (let i = 0; i < numRolls; i++) {
         const result = rollDice(6);
         if (result < 1 || result > 6) {
-            throw new Error("Test Failed for 6-sided dice roll: got " + result);
+            throw new Error("Test Failed for 6 sided dice roll: got " + result);
         }
     }
     console.log("All test rolls passed for 6 sided dice")
 }
 
+// Test 4 sided dice roll
+function testDice4() {
+    const numRolls = 100;
+    for (let i = 0; i < numRolls; i++) {
+        const result = rollDice(4);
+        if (result < 1 || result > 4) {
+            throw new Error("Test Failed for 4 sided dice roll: got " + result);
+        }
+    }
+    console.log("All test rolls passed for 4 sided dice")
+}
+
+
 testDice6();
+testDice4();
 console.log(rollDice(6));
